de9ecf53c9551705c09b2df4833c442b5a00e45e
tmm <blocklocmedia@gmail.com>
2019-08-26 18:20:37 -0400

    setup graphQL Yoga, updated datamodel.prisma

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,19 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-    dogs(parent, args, ctx, info){
-        global.dogs = global.dogs || [];
-        return global.dogs
-    },
+    items: forwardTo('db'),
+
+    
+    // dogs(parent, args, ctx, info){
+    //     global.dogs = global.dogs || [];
+    //     return global.dogs
+    // },
+
+    // used forwardto, so no need to write the following. 
+    // async items(parent,args, ctx, info){
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
 };
 
 module.exports = Query;
